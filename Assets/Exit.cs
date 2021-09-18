74173f66879c71d17b6a9945f00434e40f307eeb
rijalagusr131 <rijalrusmana131@gmail.com>
2021-09-18 08:28:48 +0700

    commit4
    commit4

---
+++ Assets/Exit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Exit : MonoBehaviour
+{
+    public AudioSource ButtonSound;
+
+    public void ExitGame()
+    {
+        AudioSource buttonsound = ButtonSound.GetComponent<AudioSource>();
+        buttonsound.PlayOneShot(buttonsound.clip);
+        Application.Quit ();
+    }
+}
